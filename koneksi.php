05e3e90d06d865e3c8c64d5c4c00690a2fdad0fa
M.Syarhan Adlil Rafi <syarhan25506@gmail.com>
2025-10-31 11:12:55 +0700

    ini versi semi final

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost","aditya","kolot123","perpustakaan",3307);
+$koneksi = mysqli_connect("localhost","root","","pustaka",3307);
 // Check connection
 if (mysqli_connect_errno()){
 echo "Koneksi database gagal : " . mysqli_connect_error();
