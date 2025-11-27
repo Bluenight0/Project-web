73d6a0df550946231ff83456cd74902bf8733b47
Bluenight0 <hamidftyula@gmail.com>
2025-11-27 17:39:40 +0700

    koneksi ku cik

--- back-end/koneksi.php
+++ back-end/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", " ", "pustaka");
+$koneksi = mysqli_connect("localhost", "root", "Aditya", "pustaka");
 
 // Check connection
 if (mysqli_connect_errno()) {
